679846f561daec64bb85305d3d7ef951e248f18d
ZahraKhademi1997 <zhra.k97@gmail.com>
2024-03-30 17:22:35 -0400

    Updating Running script in Hipergator.

--- configs/one_node_mask_MOTR_train.sh
+++ configs/one_node_mask_MOTR_train.sh
@@ -5,7 +5,7 @@
 # University of Florida Research Computing
 
 #SBATCH --wait-all-nodes=1
-#SBATCH --job-name=applemots_train_mask
+#SBATCH --job-name=applemots_train
 #SBATCH --mail-type=NONE
 #SBATCH --mail-user=
 #SBATCH --time=71:00:00
@@ -15,7 +15,7 @@
 #SBATCH --error=R-%x.%j.err
 #SBATCH --gpus-per-node=a100:8
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-gpu=8
+#SBATCH --cpus-per-gpu=4
 #SBATCH --mem-per-cpu=8gb
 #SBATCH --constraint=a100
 
@@ -27,7 +27,7 @@
 ## You can load a software environment or use a singularity container.
 ## CONTAINER="singularity exec --nv /path/to/container.sif" (--nv option is to enable gpu)
 
-module load conda/22.11.1 gcc/12.2.0
+module load conda/24.1.2 gcc/12.2.0
 conda activate seg-training
 # conda activate motr-seg
 
@@ -47,14 +47,14 @@ echo "---------------------------"
 # PYTHON SCRIPT
 #==============
 # echo "Starting $SLURM_GPUS_PER_TASK process(es) on each node..."
-# # python -u main.py
+# python -u main.py
 # python -u models/ops/setup.py build install
 
 
 # PYTHON SCRIPT
 #==============
 echo "Starting $SLURM_GPUS_PER_TASK process(es) on each node..."
-python -m torch.distributed.launch --nproc_per_node=8 \
+python -m torch.distributed.launch --nproc_per_node=4 \
     --use_env main.py \
     --meta_arch motr \
     --dataset_file e2e_joint \
@@ -64,22 +64,22 @@ python -m torch.distributed.launch --nproc_per_node=8 \
     --lr_drop 100 \
     --lr 2e-4 \
     --lr_backbone 2e-5 \
-    --lr_segmentation_bbox_attention_q_linear 2e-4 \
-    --lr_segmentation_bbox_attention_k_linear 2e-4 \
-    --lr_segmentation_mask_head_lay1 2e-4 \
-    --lr_segmentation_mask_head_gn1 2e-4 \
-    --lr_segmentation_mask_head_lay2 2e-4 \
-    --lr_segmentation_mask_head_gn2 2e-4 \
-    --lr_segmentation_mask_head_lay3 2e-4 \
-    --lr_segmentation_mask_head_gn3 2e-4 \
-    --lr_segmentation_mask_head_lay4 2e-4 \
-    --lr_segmentation_mask_head_gn4 2e-4 \
-    --lr_segmentation_mask_head_lay5 2e-4 \
-    --lr_segmentation_mask_head_gn5 2e-4 \
-    --lr_segmentation_mask_head_out_lay 2e-4 \
-    --lr_segmentation_mask_head_adapter1 2e-4 \
-    --lr_segmentation_mask_head_adapter2 2e-4 \
-    --lr_segmentation_mask_head_adapter3 2e-4 \
+    --lr_segmentation_bbox_attention_q_linear 4e-5 \
+    --lr_segmentation_bbox_attention_k_linear 4e-5 \
+    --lr_segmentation_mask_head_lay1 4e-5 \
+    --lr_segmentation_mask_head_gn1 4e-5 \
+    --lr_segmentation_mask_head_lay2 4e-5 \
+    --lr_segmentation_mask_head_gn2 4e-5 \
+    --lr_segmentation_mask_head_lay3 4e-5 \
+    --lr_segmentation_mask_head_gn3 4e-5 \
+    --lr_segmentation_mask_head_lay4 4e-5 \
+    --lr_segmentation_mask_head_gn4 4e-5 \
+    --lr_segmentation_mask_head_lay5 4e-5 \
+    --lr_segmentation_mask_head_gn5 4e-5 \
+    --lr_segmentation_mask_head_out_lay 4e-5 \
+    --lr_segmentation_mask_head_adapter1 4e-5 \
+    --lr_segmentation_mask_head_adapter2 4e-5 \
+    --lr_segmentation_mask_head_adapter3 4e-5 \
     --output_dir /blue/hmedeiros/khademi.zahra/MOTR-train/MOTR-mask-AppleMots/output/model_checkpoint/ \
     --batch_size 1 \
     --sample_mode random_interval \
